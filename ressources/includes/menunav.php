145df49f7bc057f1c8792b4995325004bb7fa1bf
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 14:10:52 +0100

    Update menunav.php

--- ressources/includes/menunav.php
+++ ressources/includes/menunav.php
@@ -1,16 +1,18 @@
-<link rel="stylesheet" type="text/css" href="/css/menunav.css">
+<link rel="stylesheet" type="text/css" href="ressources/css/menunav.css">
 <div>
-	<nav>
-		<ul>
-			<div class="right">
+	<nav class="menu-nav">
+		<div class="right">
+			<ul>
 				<li class="btn_li"><a class="btn_a" href="">Accueil</a></li>
 				<li class="btn_li"><a class="btn_a" href="">Boutique</a></li>
 				<li class="btn_li"><a class="btn_a" href="">Forum</a></li>
-			</div>
-			<div class="left">
+			</ul>
+		</div>
+		<div class="left">
+			<ul>
 				<li class="btn_li"><a class="btn_a" href="">Connexion</a></li>
 				<li class="btn_li"><a class="btn_a" href="">Inscription</a></li>
-			</div>
-		</ul>
+			</ul>
+		</div>
 	</nav>
 </div>
