0e72ab8930294f36d9e54bf21d596751f8386455
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 14:15:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<link rel="stylesheet" type="text/css" href="ressources/css/index.css">
 	<link rel="stylesheet" type="text/css" href="ressources/fontawesome/css/all.css">
