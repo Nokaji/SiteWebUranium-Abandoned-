565e70bf65444b6449445dcc6d6c2d4643ef5893
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 15:19:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta >
-    <title>Home | Uranium-PvP-Faction</title>
+    <title>Home | UraniumV1-PvP-Faction</title>
 </head>
 <header>
 	<?= include "ressources/includes/menunav.php"?>
