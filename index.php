ab778e18f0e0e7c2a74ef283044f9adc2eed0584
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 14:55:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
 	<link rel="stylesheet" type="text/css" href="ressources/css/index.css">
 	<link rel="stylesheet" type="text/css" href="ressources/fontawesome/css/all.css">
 	<link rel="stylesheet" type="text/css" href="ressources/fontawesome/css/fontawesome.css">
+	<meta property="og:image" content="ressources/img/favicon.png"/>
+	<link rel="icon" type="image/png" href="ressources/img/favicon.png"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | Uranium</title>
-    <?= include "ressources/includes/menunav.php"?>
+    <meta >
+    <title>Home | Uranium-PvP-Faction</title>
 </head>
+<header>
+	<?= include "ressources/includes/menunav.php"?>
+</header>
 <body>
-    <img src="ressources/img/theme_logo.png">
+    
 </body>
 </html>
