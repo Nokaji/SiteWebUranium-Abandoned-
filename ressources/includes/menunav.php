35ffd8175590a6c2f6c13d163a791021d7a7d35c
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 12:30:36 +0100

    Create menunav.php

---
+++ ressources/includes/menunav.php
@@ -0,0 +1,15 @@
+<div>
+	<nav>
+		<ul>
+			<div class="right">
+				<li class="btn_li"><a class="btn_a" href="">Accueil</a></li>
+				<li class="btn_li"><a class="btn_a" href="">Boutique</a></li>
+				<li class="btn_li"><a class="btn_a" href="">Forum</a></li>
+			</div>
+			<div class="left">
+				<li class="btn_li"><a class="btn_a" href="">Connexion</a></li>
+				<li class="btn_li"><a class="btn_a" href="">Inscription</a></li>
+			</div>
+		</ul>
+	</nav>
+</div>
