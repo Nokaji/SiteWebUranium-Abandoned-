868d9f5ab7f8a5f94dd7293db7a81b42034f152b
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 18:30:36 +0100

    Update menunav.php

--- ressources/includes/menunav.php
+++ ressources/includes/menunav.php
@@ -1,21 +1,23 @@
-<link rel="stylesheet" type="text/css" href="../css/menunav.css">
+<link rel="stylesheet" type="text/css" href="ressources/css/menunav.css">
 <div>
+	<div class="header-img">
 	<nav class="menu-nav">
 		<div class="div-right">
 			<ul>
 				<li class="btn_li"><a class="btn_a" href="">Accueil</a></li>
 				<li class="btn_li"><a class="btn_a" href="">Boutique</a></li>
 				<li class="btn_li"><a class="btn_a" href="">Forum</a></li>
+				<li class="btn_li"><a class="btn_a" href="">Wiki</a></li>
+				<li class="btn_li"><a class="btn_button btn_a" href="">Télécharger</a></li>
 			</ul>
 		</div>
-		<div class="div-left">
+		<!-- <div class="div-left">
 			<ul>
 				<li class="btn_li"><a class="btn_a" href="">Connexion</a></li>
 				<li class="btn_li"><a class="btn_a" href="">Inscription</a></li>
 			</ul>
-		</div>
+		</div> -->
 	</nav>
-	<div class="header-img">
+		<center><img class="img_banner" src="ressources/img/theme_logo.png"></center>
 	</div>
-	<center><img class="img_banner" src="../img/theme_logo.png"></center>
 </div>
