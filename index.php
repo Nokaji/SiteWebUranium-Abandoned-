b70ecde0854be5ed05f4d42681dd45779f9d6dc3
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 12:21:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ include
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" type="text/css" href="ressources/fontawesome/css/all.css">
+	<link rel="stylesheet" type="text/css" href="ressources/fontawesome/css/fontawesome.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Uranium</title>
