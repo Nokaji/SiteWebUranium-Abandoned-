938ea8fe4b77bdb108f37534e0c0727ddb3a9c4d
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-21 18:30:30 +0100

    Update database.php

--- ressources/database.php
+++ ressources/database.php
@@ -1,1 +1,1 @@
-<?php$bdd = new PDO("mysql:host=127.0.0.1;dbname=siteweb;charset=utf8", "root", "");?>
+<!-- <?php$bdd = new PDO("mysql:host=127.0.0.1;dbname=siteweb;charset=utf8", "root", "");?> -->
