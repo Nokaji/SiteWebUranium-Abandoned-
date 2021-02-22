48957583e81639b3231e25c73628cfb57a442b4f
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-23 00:53:06 +0100

    fontawesome

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Home | Uranium</title>
 	<meta property="og:image" content="ressources/img/favicon.png"/>
 	<link rel="icon" type="image/png" href="ressources/img/favicon.png"/>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
 </head>
 <header>
 	<?php include "ressources/includes/menunav.php"?>
--- ressources/includes/menunav.php
+++ ressources/includes/menunav.php
@@ -1,11 +1,22 @@
 <link rel="stylesheet" type="text/css" href="ressources/css/menunav.css">
-<nav class="navbar">
-	<div class="menu-btn">
+<nav class="navbar header-img">
+	<div class="navbar-menu menu">
 		<div class="navbar-start">
-			<a class="btn_a navbar-item">Accueil</a>
-			<a class="btn_a navbar-item">Forum</a>
-			<a class="btn_a navbar-item">Wiki</a>
-			<a class="btn_a navbar-item">Télécharger</a>
+			<a class="btn_a item">Accueil</a>
+			<a class="btn_a item">Forum</a>
+			<a class="btn_a item">Wiki</a>
+			<a class="btn_a item">Télécharger</a>
+		</div>
+		<div class="navbar-end">
+			<div class="navbar-item has-dropdown is-hoverable">
+				<div class="btn_item">
+					Profile<i class="fas fa-chevron-circle-down"></i>
+				</div>
+				<div class="navbar-dropdown">
+					<a class="navbar-item">Connexion</a>
+					<a class="navbar-item">Inscription</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </nav>
