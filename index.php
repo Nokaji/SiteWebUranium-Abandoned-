f909261ba3f94c0e39e8891fe7d5218e9e6c6401
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-22 23:48:17 +0100

    update

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Uranium</title>
+	<meta property="og:image" content="ressources/img/favicon.png"/>
+	<link rel="icon" type="image/png" href="ressources/img/favicon.png"/>
+</head>
+<header>
+	<?php include "ressources/includes/menunav.php"?>
+</header>
+<body>
+
+</body>
+</html>
---
+++ ressources/includes/menunav.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" type="text/css" href="ressources/css/menunav.css">
+<nav class="navbar">
+	<div class="menu-btn">
+		<a class="btn_a navbar-item">Accueil</a>
+		<a class="btn_a navbar-item">Forum</a>
+		<a class="btn_a navbar-item">Wiki</a>
+		<a class="btn_a navbar-item">Télécharger</a>
+	</div>
+</nav>
