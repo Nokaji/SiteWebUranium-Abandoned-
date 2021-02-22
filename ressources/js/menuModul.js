bc1733aae7a277e87b68471d660a313bcc64c9dc
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-22 23:21:34 +0100

    update

--- ressources/js/menuModul.js
+++
@@ -1,9 +0,0 @@
-function menuModul(){
-	document.getElementById("menu").style.visibility = "visible";
-	document.getElementById("menu-bars").style.visibility = "hidden";
-}
-
-function closeMenuModul(){
-	document.getElementById("menu").style.visibility = "hidden";
-	document.getElementById("menu-bars").style.visibility = "visible";
-}
