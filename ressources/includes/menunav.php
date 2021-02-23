a5d3c6932b9a72d2b0521b3f5f792bcb7466297e
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-23 16:31:47 +0100

    update

--- ressources/includes/menunav.php
+++ ressources/includes/menunav.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" type="text/css" href="ressources/css/menunav.css">
 <nav class="navbar nav-menu">
-	<i class="fas fa-bars bars-icon fa-5x"></i>
+	<i onclick="openMenu()" id="openMenu" class="fas fa-bars bars-icon fa-5x"></i>
+	<i onclick="closeMenu()" id="closeMenu" class="fas fa-times x-icon fa-5x"></i>
 	<div class="navbar-menu is-active menu-bars" id="navMenu">
 		<h1 class="title-menu">Menu</h1>
 		<div class="navbar-start">
@@ -8,17 +9,18 @@
 			<a class="item">Forum</a>
 			<a class="item">Wiki</a>
 			<a class="item">Télécharger</a>
-		</div>
-		<div class="navbar-end">
 			<div class="navbar-item has-dropdown is-hoverable">
 				<div class="btn_item btn_a">
 					Profile <i class="fas fa-chevron-circle-down menu-arrow"></i>
 				</div>
 				<div class="navbar-dropdown bg-dark">
-					<a class="navbar-item item" onclick="openConnexion()">Connexion</a>
-					<a class="navbar-item item">S'Inscrire</a>
+					<a class="navbar-item item drop-item" onclick="openConnexion()">Connexion</a>
+					<a class="navbar-item item drop-item">S'Inscrire</a>
 				</div>
 			</div>
+		</div>
+		<div class="navbar-end">
+			<a class="item btn-shop">Boutique</a>
 		</div>
 	</div>
 </nav>
---
+++ ressources/js/menu.js
@@ -0,0 +1,11 @@
+function openMenu(){
+	document.getElementById("navMenu").style.visibility = "visible";
+	document.getElementById("openMenu").style.visibility = "hidden";
+	document.getElementById("closeMenu").style.visibility = "visible";
+}
+
+function closeMenu(){
+	document.getElementById("navMenu").style.visibility = "hidden";
+	document.getElementById("closeMenu").style.visibility = "hidden";
+	document.getElementById("openMenu").style.visibility = "visible";
+}
