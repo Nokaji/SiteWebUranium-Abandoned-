7e97eec725279e4ade1ea6a47cab559264434b0a
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-23 01:39:53 +0100

    update

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
 	<meta property="og:image" content="ressources/img/favicon.png"/>
 	<link rel="icon" type="image/png" href="ressources/img/favicon.png"/>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+	<link rel="stylesheet" type="text/css" href="ressources/fontawesome/css/all.css">
 </head>
 <header>
 	<?php include "ressources/includes/menunav.php"?>
+	
 </header>
 <body>
 
--- ressources/includes/menunav.php
+++ ressources/includes/menunav.php
@@ -1,16 +1,21 @@
 <link rel="stylesheet" type="text/css" href="ressources/css/menunav.css">
 <nav class="navbar header-img">
-	<div class="navbar-menu menu">
+	<a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+		<span></span>
+		<span></span>
+		<span></span>
+	</a>
+	<div class="navbar-menu is-active menu-bars" id="navMenu">
 		<div class="navbar-start">
-			<a class="btn_a item">Accueil</a>
-			<a class="btn_a item">Forum</a>
-			<a class="btn_a item">Wiki</a>
-			<a class="btn_a item">Télécharger</a>
+			<a class="navbar-item btn_a">Accueil</a>
+			<a class="navbar-item btn_a">Forum</a>
+			<a class="navbar-item btn_a">Wiki</a>
+			<a class="navbar-item btn_a">Télécharger</a>
 		</div>
 		<div class="navbar-end">
 			<div class="navbar-item has-dropdown is-hoverable">
 				<div class="btn_item">
-					Profile<i class="fas fa-chevron-circle-down"></i>
+					Profile <i class="fas fa-chevron-circle-down menu-arrow"></i>
 				</div>
 				<div class="navbar-dropdown">
 					<a class="navbar-item">Connexion</a>
@@ -20,3 +25,5 @@
 		</div>
 	</div>
 </nav>
+<center><img src="ressources/img/favicon.png" style="max-width: 10%; height: auto; top: -10px;"></center>
+<script type="text/javascript" src="ressources/js/menu.js"></script>
