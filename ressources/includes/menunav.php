5d8b66e17f2e366d3f4be3b1f815588cd35b61b4
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-23 15:19:43 +0100

    update

--- ressources/includes/menunav.php
+++ ressources/includes/menunav.php
@@ -1,11 +1,7 @@
 <link rel="stylesheet" type="text/css" href="ressources/css/menunav.css">
-<nav class="navbar nav-menu is-active">
-	<a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
-  		<span></span>
-	  	<span></span>
-	  	<span></span>
-	</a>
+<nav class="navbar nav-menu">
 	<div class="navbar-menu is-active menu-bars" id="navMenu">
+		<h1 class="title-menu" style="color: #00A810; visibility: hidden; position: absolute; top: 25px; right: 25px;">Menu</h1>
 		<div class="navbar-start">
 			<a class="item">Accueil</a>
 			<a class="item">Forum</a>
@@ -18,8 +14,8 @@
 					Profile <i class="fas fa-chevron-circle-down menu-arrow"></i>
 				</div>
 				<div class="navbar-dropdown bg-dark">
-					<a class="navbar-item" onclick="openConnexion()">Connexion</a>
-					<a class="navbar-item">S'Inscrire</a>
+					<a class="navbar-item item" onclick="openConnexion()">Connexion</a>
+					<a class="navbar-item item">S'Inscrire</a>
 				</div>
 			</div>
 		</div>
