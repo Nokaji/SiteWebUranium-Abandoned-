6f24be9b174772ef4e2b206512affd658d9cdaea
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-23 20:32:18 +0100

    update

--- ressources/includes/base.php
+++ ressources/includes/base.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="ressources/css/base.css">
 <div class="play">
 	<span>
-		<p class="title-play">Rejoins nous avec plus de <em class="p-play">100</em> Joueurs !!</p>
+		<p>Rejoins nous avec plus de <em class="p-play">100</em> Joueurs !!</p>
 		<a class="btn-play">Jouer</a>
 	</span>
 </div>
