a4f447978b32898f900e4d63a5d7fe9d2ff51b98
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-24 20:24:15 +0100

    update

--- ressources/includes/inscription.php
+++ ressources/includes/inscription.php
@@ -1,4 +1,55 @@
-<!--	<div id="inscription" class="box">
+<?php
+include "ressources/database.php";
+ 
+if(isset($_POST['forminscription'])) {
+   $pseudo = htmlspecialchars($_POST['pseudo']);
+   $mail = htmlspecialchars($_POST['email']);
+   $mail2 = htmlspecialchars($_POST['mail2']);
+   $mdp = sha1($_POST['password']);
+   $mdp2 = sha1($_POST['passwordc']);
+   if(!empty($_POST['pseudo']) AND !empty($_POST['email']) AND !empty($_POST['mail2']) AND !empty($_POST['password']) AND !empty($_POST['passwordc'])) {
+      $pseudolength = strlen($pseudo);
+      $mdplength = strlen($mdp);
+      if($pseudolength <= 255) {
+      		$reqpseudo = $bdd->prepare("SELECT * FROM membres WHERE pseudo = ?");
+      		$reqpseudo->execute(array($pseudo));
+      		$pseudoexist = $reqpseudo->rowCount();
+      		if($pseudoexist == 0) {
+         if($mail == $mail2) {
+            if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+               $reqmail = $bdd->prepare("SELECT * FROM membres WHERE mail = ?");
+               $reqmail->execute(array($mail));
+               $mailexist = $reqmail->rowCount();
+               if($mailexist == 0) {
+                  if($mdp == $mdp2) {
+                     $insertmbr = $bdd->prepare("INSERT INTO membres(pseudo, mail, motdepasse) VALUES(?, ?, ?)");
+                     $insertmbr->execute(array($pseudo, $mail, $mdp));
+                     $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
+                  } else {
+                     $erreur = "Vos mots de passes ne correspondent pas !";
+                  }
+               } else {
+                  $erreur = "Adresse mail déjà utilisée !";
+               }
+            } else {
+               $erreur = "Votre adresse mail n'est pas valide !";
+            }
+         } else {
+            $erreur = "Vos adresses mail ne correspondent pas !";
+         }
+     	} else {
+     		$erreur = "Ce pseudo exist déjà !";
+     }
+      } else {
+         $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
+      }
+   } else {
+      $erreur = "Tous les champs doivent être complétés !";
+   }
+}
+?>
+
+<div id="inscription" class="box">
 	<center><h1 class="title is-1" style="color: white;">Inscription</h1></center>
     <span class="iconclose icon is-medium"><i onclick="closeInscription()" class="fas fa-times-circle"></i></span>
 	<form method="POST" action="" class="box" style="background: transparent;">
@@ -33,4 +84,7 @@
 			</div>
 		</div>
 	</form>
-</div> !-->
+</div>
+
+<link rel="stylesheet" type="text/css" href="ressources/css/inscription.css">
+<script type="text/javascript" src="ressources/js/popupInscription.js"></script>
