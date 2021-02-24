3f124d7c001cb310aa9ed01ee2eb13d06269a444
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-24 14:45:02 +0100

    update

--- inscription.php
+++ inscription.php
@@ -10,12 +10,46 @@
 	<?php include "ressources/includes/menunav.php"?>
 </head>
 <header>
-	<?php include "ressources/includes/header.php" ?>
+	<?php include "ressources/includes/header2.php" ?>
 </header>
 <body>
 	<?php include "ressources/includes/download.php" ?>
 	<?php include "ressources/includes/connexion.php" ?>
-	<?php include "ressources/includes/base.php"?>
-
+	<div id="inscription" class="box">
+	<center><h1 class="title is-1" style="color: white;">Inscription</h1></center>
+    <span class="iconclose icon is-medium"><i onclick="closeInscription()" class="fas fa-times-circle"></i></span>
+	<form method="POST" action="" class="box" style="background: transparent;">
+		<div class="field"">
+			<div class="control">
+				<center><label style="color: white;" class="label left">Pseudo Minecraft :</label>
+				<input name="pseudo" id="pseudo" class="input input-inscri" type="text" placeholder="Exemple : Pseudo1234" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>"></input></center>
+			</div>
+			<div class="control">
+				<center><label style="color: white;" class="label left">Email :</label>
+				<input name="email" id="email" class="input input-inscri" type="email" placeholder="Exemple : email@gmail.com" value="<?php if(isset($email)) { echo $email; } ?>"></input></center>
+			</div>
+			<div class="control">
+				<center><label style="color: white;" class="label left">Email :</label>
+				<input name="mail2" id="mail2" class="input input-inscri" type="email" placeholder="Comfirmation de l'email !" value="<?php if(isset($mail2)) { echo $mail2; } ?>"></input></center>
+			</div>
+			<div class="control">
+				<center><label style="color: white;" class="label left">Mot de Passe :</label>
+				<input name="password" id="password" class="input input-inscri" type="password" placeholder="********************"></input></center>
+			</div>
+			<div class="control">
+				<center><label style="color: white;" class="label left">Comfirmation Mot de Passe :</label>
+				<input name="passwordc" id="passwordc" class="input input-inscri" type="password" placeholder="********************"></input></center>
+			</div><br />
+			<div class="control">
+				<center><input name="forminscription" type="submit" class="button is-primary" value="S'inscire"/></center>
+				<?php
+				if(isset($erreur)) {
+					echo '<font color="red">'.$erreur."</font>";
+				}
+				?>
+			</div>
+		</div>
+	</form>
+</div>
 </body>
 </html>
--- ressources/includes/header2.php
+++ ressources/includes/header2.php
@@ -1,4 +1,4 @@
 <div class="header-background overlay">
 	<center><img class="header-logo image" src="ressources/img/banner.png"></img></center>
 </div>
-<link rel="stylesheet" type="text/css" href="ressources/css/header.css">
+<link rel="stylesheet" type="text/css" href="ressources/css/header2.css">
