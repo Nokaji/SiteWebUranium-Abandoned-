a361bd53cee1e8b03decdff2ab5dbdb842767b93
Proxi8465 <71786224+Proxi8465@users.noreply.github.com>
2021-02-25 19:57:31 +0100

    compte

--- connexion.php
+++ connexion.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta property="og:image" content="ressources/img/favicon.png"/>
+	<link rel="icon" type="image/png" href="ressources/img/favicon.png"/>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+	<link rel="stylesheet" type="text/css" href="ressources/fontawesome/css/all.css">
+	<link rel="stylesheet" type="text/css" href="ressources/css/connexion.css">
+	<title>Connexion | UraniumV1</title>
+	<?php include "ressources/includes/menunav.php"?>
+</head>
+<header>
+	<?php include "ressources/includes/header2.php" ?>
+</header>
+<body>
+	<?php include "ressources/includes/download.php" ?>
+	
+<div class="retour">
+	<a class="retour-accueil">Accueil</a><p class="retour-p">></p>
+	<h1 class="title is-1 title-perso" style="color: white;">Inscription</h1>
+</div>
 <div id="connexion">
 	<h1 class="title is-1" style="color: white;">Connexion</h1>
 	<span class="iconclose icon is-medium"><i onclick="closeConnexion()" class="fas fa-times-circle"></i></span>
@@ -17,5 +41,5 @@
 		</div>
 	</form>
 </div>
-<link rel="stylesheet" type="text/css" href="ressources/css/connexion.css">
-<script type="text/javascript" src="ressources/js/popupConnexion.js"></script>
+</body>
+</html>
